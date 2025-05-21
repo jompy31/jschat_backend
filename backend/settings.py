ee4f5e87c58c749dbf00feac1b7e8d6b9b1904b5
jompy31 <jpbcserviciotecnico@gmail.com>
2025-05-21 14:51:12 -0600

    settings

--- backend/settings.py
+++ backend/settings.py
@@ -20,7 +20,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/api/media/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Application definition
 INSTALLED_APPS = [
@@ -134,14 +137,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# Static files
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
